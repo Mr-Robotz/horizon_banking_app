59dfa1afbe430a0864f62355b19b59d3ef600ebf
Mr Robotz <mrr.robotz@gmail.com>
2024-09-17 18:09:52 -0700

    Create all the pages for the banking-app and also integrated the material-tailwind library in the banking-app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,45 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+        manRope: ["Manrope", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primaryColor: "#004e89",
+        lighterGray: "#f3f9ff",
+        lightgray: "#e5edf3",
+        buttonColor: "#1b96f3",
+        darkBlueColor: "#192a56",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1.5rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
       },
     },
   },
   plugins: [],
 };
-export default config;
+
+module.exports = withMT(config);
